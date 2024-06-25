ede6c89d9bd0c6349c0e015e6c0beb8e5b271b6e
ayrisdev <ayris.dev@gmail.com>
2024-06-25 19:09:25 +0300

    first commit

--- api.js
+++ api.js
@@ -1,7 +1,6 @@
-const { Telegraf } = require("telegraf");
-const fetch = require("node-fetch");
-require("dotenv").config();
-
+import fetch from "node-fetch";
+import dotenv from "dotenv";
+dotenv.config();
 const WEBHOOK_URL = process.env.WEBHOOK_URL;
 export async function getEmptyDatesFromApi() {
   const response = await fetch(`${WEBHOOK_URL}/api/checkdate`);
