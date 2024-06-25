2c9ea4509587f103ece7df4bf02479828cab3a17
ayrisdev <ayris.dev@gmail.com>
2024-06-25 22:30:08 +0300

    first commit

--- api.js
+++ api.js
@@ -2,7 +2,7 @@ import fetch from "node-fetch";
 
 export async function getEmptyDatesFromApi() {
   const response = await fetch(
-    "https://new-express-project-omega-nine.vercel.app/api/checkdate"
+    "https://vercelnode-kappa.vercel.app/api/checkdate"
   );
   if (!response.ok) {
     throw new Error(`Error fetching data from API: ${response.statusText}`);
