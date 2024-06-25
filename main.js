89c13ffbca4a4036033ff720298f98625009557e
ayrisdev <ayris.dev@gmail.com>
2024-06-25 16:20:00 +0300

    first commit

--- main.js
+++ main.js
@@ -1,6 +1,6 @@
 import { Telegraf, session } from "telegraf";
 import express from "express";
-import { format, parseISO } from "date-fns";
+import { format, parseISO, isValid } from "date-fns";
 import dotenv from "dotenv";
 import {
   fetchNotionDatabase,
